70ea3fe4f23f6322df7da4897117d4c0ba12d175
Akash Trehan <akash.trehan123@gmail.com>
2016-08-13 18:58:18 +0530

    Added new lend

--- lendit/website/views.py
+++ lendit/website/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from website.models import *
 
 # Create your views here.
 def home(request):
@@ -6,4 +8,15 @@ def home(request):
 
 
 def lend(request):
-	return render(request, 'new_lend.html', {})
+	if(request.method == "GET"):
+		return render(request, 'new_lend.html', {})
+	if(request.method == "POST"):
+		name = request.POST['name']
+		condition = request.POST['condition']
+		desc = request.POST['desc']
+		tfl = request.POST['tfl']
+		url = request.POST['url']
+		book = Book.objects.filter(name=name)[0]
+		user = request.user
+		UserBook(desc=desc, lending_time=tfl, image_url=url, condition=condition, orig_book=book, user=user).save()
+		return HttpResponse(request.POST['name'])
